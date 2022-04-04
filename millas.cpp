2ddfe9977b45f328db4dcb9aaf4725ab7f2ec6b6
HURTADO MENDEZ Arturo <hurtadomendez90@gmail.com>
2022-04-04 18:10:01 -0500

    Agregando distancia a kilometros

--- millas.cpp
+++ millas.cpp
@@ -4,15 +4,25 @@ int main (){
 
     int millas;
     const int millaMarina = 1852;
+    const int kilometros = 1000;
     int total;
+
+    int totalKilometros;
+    int totalMetros;
 
     cout << "Ingresa una distancia en millas: ";
     cin >> millas;
 
     total = millas * millaMarina;
 
-    cout << "La distancia en millas es de: " << millas << " la distancia en metros es de: " << total    ;
+    cout << "La distancia es de: " << millas << " millas" << endl; 
+    cout << "la distancia en metros es de: " << total << endl;
 
+
+    totalKilometros = total / kilometros;
+    totalMetros = total % kilometros;
+
+    cout << "La distancia en kilometros es de: " << totalKilometros << " kilometros y " << totalMetros << " metros.";
 
     return 0;
-}
+}   
